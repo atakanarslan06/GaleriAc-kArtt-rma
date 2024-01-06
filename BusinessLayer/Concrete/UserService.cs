7e186dbd14e8440f038501982ab79365831c9274
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-06 09:06:03 +0300

    ctor

--- BusinessLayer/Concrete/UserService.cs
+++ BusinessLayer/Concrete/UserService.cs
@@ -1,6 +1,11 @@
-﻿using BusinessLayer.Abstraction;
+﻿using AutoMapper;
+using BusinessLayer.Abstraction;
 using BusinessLayer.Dtos;
 using Core.Models;
+using DataAccesLayer.Context;
+using DataAccesLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +16,23 @@ namespace BusinessLayer.Concrete
 {
     public class UserService : IUserService
     {
+        private readonly DbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly ApiResponse _response;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private string secretKey;
+        public UserService(RoleManager<IdentityRole> roleManager, IConfiguration _configuration,
+            DbContext dbContext, IMapper mapper, ApiResponse response, UserManager<ApplicationUser> userManager )
+        {
+            _roleManager = roleManager;
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _response = response;
+            _roleManager = roleManager;
+            secretKey = _configuration.GetValue<string>("SecretKey: jwtKey");
+        }
+
         public Task<ApiResponse> Login(LoginRequestDTO model)
         {
             throw new NotImplementedException();
