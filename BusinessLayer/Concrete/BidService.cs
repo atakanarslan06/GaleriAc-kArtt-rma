09f9646a4b7a91d521bdfbac71f351678cf26431
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-06 18:52:09 +0300

    BidServ

---
+++ BusinessLayer/Concrete/BidService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BusinessLayer.Abstraction;
+using BusinessLayer.Dtos;
+using Core.Models;
+using DataAccesLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    internal class BidService : IBidService
+    {
+        public BidService(ApplicationDbContext context, IMapper mapper, ApiResponse response)
+        {
+            _context = context;
+        }
+
+        public ApplicationDbContext _context { get; }
+
+        public Task<ApiResponse> AutomaticallyCreateBid(CreateBidDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> CancelBid(int bidId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> CreateBid(CreateBidDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> GetBidById(int bidId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> GetBidByVehicleId(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> UpdateBid(int bidId, UpdateBidDTO model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLayer/Dtos/CreateBidDTO.cs
@@ -0,0 +1,19 @@
+﻿using DataAccesLayer.Domain;
+using DataAccesLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Dtos
+{
+    public class CreateBidDTO
+    {
+        public decimal BidAmount { get; set; }
+        public DateTime BidDate { get; set; }
+        public string? BidStatus { get; set; } 
+        public string? UserId { get; set; }
+        public int VehicleId { get; set; }
+    }
+}
