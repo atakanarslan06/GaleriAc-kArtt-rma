5dd4a95f1b7b68c3738e5ccbb927e0ea102682fe
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-06 09:45:02 +0300

    loginservice

--- BusinessLayer/Concrete/UserService.cs
+++ BusinessLayer/Concrete/UserService.cs
@@ -7,6 +7,10 @@ using DataAccesLayer.Enums;
 using DataAccesLayer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BusinessLayer.Concrete
 {
@@ -29,9 +33,50 @@ namespace BusinessLayer.Concrete
             secretKey = _configuration.GetValue<string>("SecretKey: jwtKey");
         }
 
-        public Task<ApiResponse> Login(LoginRequestDTO model)
+        public async Task<ApiResponse> Login(LoginRequestDTO model)
         {
-            throw new NotImplementedException();
+            ApplicationUser userFromDb = _dbContext.applicationUsers.FirstOrDefault(u=>u.UserName.ToLower() == model.UserName.ToLower());
+            if (userFromDb != null)
+            {
+                bool isValid = await _userManager.CheckPasswordAsync(userFromDb, model.Password);
+                if (!isValid)
+                {
+                    _response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                    _response.ErrorMessages.Add("Giriş Bilgileri Hatalı");
+                    _response.İsSuccess = false;
+                    return _response;
+                }
+                var role = await _userManager.GetRolesAsync(userFromDb);
+                JwtSecurityTokenHandler tokenHandler = new();
+                byte[] key = Encoding.ASCII.GetBytes(secretKey);
+
+                SecurityTokenDescriptor tokenDescriptor = new()
+                {
+                    Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, userFromDb.Id),
+                        new Claim(ClaimTypes.Email, userFromDb.Email),
+                        new Claim(ClaimTypes.Role, role.FirstOrDefault()),
+                        new Claim("fullName", userFromDb.FullName),
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+
+                };
+                SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+                LoginResponseModel _model = new()
+                {
+                    Email = userFromDb.Email,
+                    Token = tokenHandler.WriteToken(token),
+                };
+                _response.Result = _model;
+                _response.İsSuccess = true;
+                _response.StatusCode = System.Net.HttpStatusCode.OK;
+                return _response;
+            }
+            _response.İsSuccess = false;
+            _response.ErrorMessages.Add("Ooops! bir şeyler yanlış gitti");
+            return _response;
         }
 
         public async Task<ApiResponse> Register(RegisterRequestDTO model)
