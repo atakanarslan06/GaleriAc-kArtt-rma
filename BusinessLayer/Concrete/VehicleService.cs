f1c385dbf13febe3b319630e368831aaafd573f3
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-06 12:46:56 +0300

    VehicleService

---
+++ BusinessLayer/Concrete/VehicleService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BusinessLayer.Dtos;
+using Core.Models;
+using DataAccesLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class VehicleService : IVehicleService
+    {
+        private readonly DbContext _dbContext;
+        private readonly IMapper _mapper;
+        public VehicleService(DbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<ApiResponse> ChangeVehicleStatus(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> CreateVehicle(CreateVehicleDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> DeleteVehicleResponse(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> GetVehicle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> GetVehicleById(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> UpdateVehicleResponse(int vehicleId, UpdateVehicleDTO model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLayer/IVehicleService.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Dtos;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface IVehicleService
+    {
+        Task<ApiResponse> CreateVehicle(CreateVehicleDTO model);
+        Task<ApiResponse> GetVehicle();
+        Task<ApiResponse> UpdateVehicleResponse(int vehicleId, UpdateVehicleDTO model);
+        Task<ApiResponse> DeleteVehicleResponse(int vehicleId);
+        Task<ApiResponse> GetVehicleById(int vehicleId);
+        Task<ApiResponse> ChangeVehicleStatus(int vehicleId);
+    }
+}
